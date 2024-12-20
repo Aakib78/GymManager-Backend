04d971a3637e42d506f4eb79c5ef33206bc5d715
AakibSaifi <akhan@calance.com>
2024-12-20 20:52:27 +0530

    Created success and error response utilities.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import dotenv from 'dotenv';
 import connectDB from './db/db_connection.js';
-
-import express from 'express';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 });
 
-connectDB();
+connectDB().then(() => {
+    console.log('Database connected');
 
-const app = express();
+    app.on('error', (error) => {
+        console.log('ERRR: ', error);
+        throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((error) => {
+    console.log('Error connecting to database:', error);
+});
+
 
