00a39fdcad2b233061af00757f80ef3b64c0fa77
AakibSaifi <akhan@calance.com>
2025-03-10 19:05:03 +0530

    Modified Stop Script.

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-#Stopping existing node servers
+# Stopping existing Node.js servers
 echo "Stopping any existing node servers"
-pkill node
+
+# Suppress error if no Node process is found
+pkill node || echo "No Node.js process was running"
